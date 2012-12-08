aa02090ebb1b44a8d963d7f9657c5bcdfd7f5e2c
Jonathan Ku <jonathanku88@gmail.com>
2012-12-08 16:53:31 -0500

    jony bananas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioFinalRails::Application.config.secret_token = '84690fdbc436ad56dc6eb45a80c1ae0447f439472a3166cdb2dc6fd00753ffd46fa7857ad9ed9cd692bb2a6b2d36273c8d0b6af783d4459ad6c68e0adcb81662'
